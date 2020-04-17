f1413c47864c1bde9f6f095467b5e5497c1c9a6d
arjun391c <arjun391c@gmail.com>
2020-04-17 18:28:28 +0530

    passport jwt

--- conFusionServer/routes/leaderRouter.js
+++ conFusionServer/routes/leaderRouter.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-
+var authenticate = require('../authenticate');
 const Leaders = require('../models/leaders');
 
 const leaderRouter = express.Router();
@@ -19,7 +19,7 @@ leaderRouter.route('/')
             .catch((err) => next(err));
     })
 
-    .post((req, res, next) => {
+    .post(authenticate.verifyUser,(req, res, next) => {
         Leaders.create(req.body)
             .then((leader) => {
                 res.statusCode = 200;
@@ -29,12 +29,12 @@ leaderRouter.route('/')
             .catch((err) => next(err));
     })
 
-    .put((req, res, next) => {
+    .put(authenticate.verifyUser,(req, res, next) => {
         res.statusCode = 403;
         res.end('PUT operation not supported on /leaders');
     })
  
-    .delete((req, res, next) => {
+    .delete(authenticate.verifyUser,(req, res, next) => {
         Leaders.remove({})
             .then((resp) => {
                 res.status = 200;
@@ -57,7 +57,7 @@ leaderRouter.route('/:leaderId')
         
     })
 
-    .post( (req, res, next) => {
+    .post(authenticate.verifyUser, (req, res, next) => {
     res.statusCode = 403;
     res.end('POST operation not supported on /leaders/'+ req.params.leaderId);
     })
@@ -77,7 +77,7 @@ leaderRouter.route('/:leaderId')
         
     })
 
-    .delete((req, res, next) => {
+    .delete(authenticate.verifyUser,(req, res, next) => {
         Leaders.findByIdAndRemove(req.params.leaderId)
         .then((resp) => {
             res.statusCode = 200;
--- conFusionServer/routes/promoRouter.js
+++ conFusionServer/routes/promoRouter.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+var authenticate = require('../authenticate');
 
 const Promotions = require('../models/promotions');
 
@@ -19,7 +20,7 @@ promoRouter.route('/')
             .catch((err) => next(err));    
     })
 
-    .post((req, res, next) => {
+    .post(authenticate.verifyUser,(req, res, next) => {
         Promotions.create(req.body)
             .then((promotion) =>{
                 res.statusCode = 200;
@@ -29,12 +30,12 @@ promoRouter.route('/')
             .catch((err) => next(err)); 
     })
 
-    .put((req, res, next) => {
+    .put(authenticate.verifyUser,(req, res, next) => {
         res.statusCode = 403;
         res.end('PUT operation not supported on /promotions');
     })
  
-    .delete((req, res, next) => {
+    .delete(authenticate.verifyUser,(req, res, next) => {
         Promotions.remove({})
             .then((resp) => {
                 res.statusCode = 200;
@@ -54,15 +55,15 @@ promoRouter.route('/:promotionId')
             }, (err) => next(err))
             .catch(err => next(err));
     })
-    .post((req, res) => {
+    .post(authenticate.verifyUser,(req, res) => {
         res.status(403);
         res.end(`POST operation not supported on /promotions/${req.params.promotionId}`);
     })
-.post((req, res, next) => {
+.post(authenticate.verifyUser,(req, res, next) => {
         res.statusCode = 403;
         res.end('POST operation not supported on /promotions/'+ req.params.promorionId);
 })
-.put((req, res, next) => {
+.put(authenticate.verifyUser,(req, res, next) => {
         Promotions.findByIdAndUpdate(req.params.promotionId, {
             $set: req.body
         }, { new: true })
@@ -73,7 +74,7 @@ promoRouter.route('/:promotionId')
         }, (err) => next(err))
         .catch((err) => next(err));
 })
-.delete((req, res, next) => {
+.delete(authenticate.verifyUser,(req, res, next) => {
         Promotions.findByIdAndRemove(req.params.promotionId)
         .then((resp) => {
             res.statusCode = 200;
